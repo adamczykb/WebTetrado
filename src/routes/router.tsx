b4de561a637479ee3a5c6660fa6262ef1f57dd29
Bartosz Adamczyk <adamczyk.b@protonmail.com>
2022-01-27 19:35:37 +0100

    init

---
+++ src/routes/router.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from "react";
+import {BrowserRouter, Routes} from "react-router-dom";
+
+
+const IndexRouter: React.FC = (): ReactElement => {
+    return (
+        <BrowserRouter>
+            <Routes>
+           
+            </Routes>
+        </BrowserRouter>
+    )
+}  
