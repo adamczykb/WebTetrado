51e702e098d6f58203d42f5c0deba9cd0a9c6ee1
BArtosz Adamczyk <adamczyk.b@protonmail.com>
2022-08-17 19:17:21 +0200

    1.2 Update Aug | task.py fix

--- backend/tasks.py
+++ backend/tasks.py
@@ -2,11 +2,11 @@ from webTetrado.celery import app
 from backend.models import TetradoRequest,Nucleotide,TemporaryFile,Log
 from datetime import datetime as dt
 import datetime
-
+import traceback
 @app.task()
 def remove_old_queries(args=None):
     try:
-        TemporaryFile.objects.filter(timestamp__lte=dt.today() - datetime.timedelta(days=1), removable=True).delete()
+        TemporaryFile.objects.filter(timestamp__lte=dt.today() - datetime.timedelta(days=1)).delete()
         for request in TetradoRequest.objects.filter(timestamp__lte=dt.today() - datetime.timedelta(days=7)):
             for helice in request.helice.all():
                 helice.tetrad_pair.all().delete()
