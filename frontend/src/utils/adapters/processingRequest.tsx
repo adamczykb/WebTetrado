3fb647532e55d7a9a018e1b8be07fceee57d5392
Bartosz Adamczyk <adamczyk.b@protonmail.com>
2023-01-01 19:31:02 +0100

    docker compose update

--- frontend/src/utils/adapters/processingRequest.tsx
+++ frontend/src/utils/adapters/processingRequest.tsx
@@ -5,45 +5,45 @@ import lang from "../../lang.json";
 import fetch from "node-fetch";
 
 type form_values = {
-  fileId: string;
-  rcsbPdbId: string;
-  settings: {
-    complete2d: boolean;
-    reorder: boolean;
-    stackingMatch: number;
-    strict: boolean;
-    model: number;
-  };
+    fileId: string;
+    rcsbPdbId: string;
+    settings: {
+        complete2d: boolean;
+        reorder: boolean;
+        stackingMatch: number;
+        strict: boolean;
+        model: number;
+    };
 };
 export function processingRequest(data: form_values, setLoading: any) {
-  const requestOptions = {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "X-CSRFToken": getCookie(),
-    },
-  };
-  requestOptions.headers["Access-Control-Allow-Origin"] = "*";
-  fetch(config.SERVER_URL + "/api/process/request/", requestOptions)
-    .then((response: any) => {
-      if (response.status == 404) {
-        message.error(lang.rcsb_error);
-        setLoading(false);
-        return "";
-      } else {
-        return response.json();
-      }
-    })
-    .then((response: any) => {
-      if (response != "") {
-        window.open(
-          config.FRONTEND_URL + "/result/" + response.orderId,
-          "_self"
-        );
-        setLoading(false);
-      }
-    })
-    .catch((error: any) => message.error("Request error: \n" + error.message));
+    const requestOptions = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "X-CSRFToken": getCookie(),
+        },
+    };
+    requestOptions.headers["Access-Control-Allow-Origin"] = "*";
+    fetch(config.SERVER_URL + "/api/process/request/", requestOptions)
+        .then((response: any) => {
+            if (response.status == 404) {
+                message.error(lang.rcsb_error);
+                setLoading(false);
+                return "";
+            } else {
+                return response.json();
+            }
+        })
+        .then((response: any) => {
+            if (response != "") {
+                window.open(
+                    config.FRONTEND_URL + "/result/" + response.orderId,
+                    "_self"
+                );
+                setLoading(false);
+            }
+        })
+        .catch((error: any) => message.error("Something went wrong, try again"));
 }
